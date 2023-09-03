2f81c89ef50ecede46145ad799d47a59ae8be19e
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-03 13:28:52 -0700

    method setup

---
+++ src/TicTacToe.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe implements ActionListener {
+
+
+    TicTacToe() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+}
