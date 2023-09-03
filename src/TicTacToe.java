ab751e1c273c2766d292f7171595159bcecbaf82
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-03 13:41:45 -0700

    added buttons

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -29,6 +29,24 @@ public class TicTacToe implements ActionListener {
         textField.setText("Tic Tac Toe");
         textField.setOpaque(true);
 
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0, 0, 800, 100);
+        titlePanel.add(textField);
+
+
+        buttonPanel.setLayout(new GridLayout(3, 3));
+        buttonPanel.setBackground(new Color(150, 150, 150));
+
+        for(int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            buttonPanel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+
+        frame.add(titlePanel, BorderLayout.NORTH);
+        frame.add(buttonPanel);
     }
 
     @Override
